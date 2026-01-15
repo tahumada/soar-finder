ec338203026fe75f2450a270b6efa013498c4dca
Tomas Ahumada <tfahumada94@gmail.com>
2026-01-15 16:29:06 -0300

    added new features

--- finder.py
+++ finder.py
@@ -26,99 +26,17 @@ from astropy.visualization import ImageNormalize, ZScaleInterval
 from reproject import reproject_interp
 
 # utils
-from utils import query_stars_gaia, query_stars_ps1, query_stars_ls, get_url, parse_coords
+from utils import query_stars_gaia, query_stars_ps1, query_stars_ls, get_stars_2mass
+from utils import get_url, parse_coords
+from utils import get_image_ps1, get_image_ls, get_image_decaps, get_image_dss, get_image_2mass,get_image_fallbacks
+from utils import setup_logger
 
 
 # Matplotlib defaults
 mpl.rcParams["font.size"] = 15  # default = 10.0
+    
 
-def get_image_ps1(ra,dec,source_name,imsize=6):
-    
-    PS1_CUTOUT_TIMEOUT = 30
-    pixscale = 0.258
-
-    numpix = int(round(60 * imsize / pixscale))
-    if numpix % 2 == 0:
-        numpix += 1
-
-    # numpix = math.ceil(60 * imsize / pixscale)
-    
-    ps_query_url = (
-        f"http://ps1images.stsci.edu/cgi-bin/ps1cutouts"
-        f"?pos={ra:.8f}+{dec:.8f}&"
-        f"filter=r&filetypes=stack&size={numpix}"
-    )
-    
-    try:
-        response = requests.get(ps_query_url, timeout=PS1_CUTOUT_TIMEOUT)
-        # see models.py for how this URL is constructed
-        match = re.search('src="//ps1images.stsci.edu.*?"', response.content.decode())
-        if match is None:
-            print(f"PS1 image not found for {ra} {dec}")
-            return ""
-        url = match.group().replace('src="', "http:").replace('"', "")
-        url += f"&format=fits&imagename=ps1{ra}{dec:+f}.fits"
-    except (requests.exceptions.SSLError, requests.exceptions.ReadTimeout) as e:
-        print(f"Error getting PS1 image URL {str(e)}")
-        return ""
-    except Exception as e:
-        print(f"Error getting PS1 image URL {e.message}")
-        return ""
-    
-    response = get_url(url, stream=True, allow_redirects=True)
-    
-    if response is None or response.status_code != 200:
-        print('failed')
-    
-    # Check if HDU is a valid FITS file
-    hdu = fits.open(io.BytesIO(response.content))
-   
-    hdu[0].header['watermark'] =  'PS1'
-    hdu[0].header['pixscale'] =  pixscale
-    hdu[0].header['imsize'] =  imsize
-    hdu[0].header['source_name'] =  source_name
-    hdu[0].header['ra'] =  ra
-    hdu[0].header['dec'] =  dec
-    hdu[0].header['numpix'] =  numpix
-    
-    return hdu
-
-
-def get_image_ls(ra,dec,source_name,
-                  imsize = 6# in arcmin
-                 ):
-    
-    pixscale = 0.26
-    LS_CUTOUT_TIMEOUT = 30
-    
-    numpix = math.ceil(60 * imsize / pixscale)
-    
-    ls_query_url = (
-         f"http://legacysurvey.org/viewer/fits-cutout/"
-         f"?ra={ra}&dec={dec}&layer=dr8&pixscale={pixscale}&bands=r&size={numpix}"
-    )
-    print(ls_query_url)
-    
-    response = requests.get(ls_query_url, timeout=LS_CUTOUT_TIMEOUT)
-    if response is None or response.status_code != 200:
-        print('failed')
-        return ''
-    
-    
-    hdu = fits.open(BytesIO(response.content))
-    hdu[0].header['watermark'] =  'LS'
-    hdu[0].header['pixscale'] =  pixscale
-    hdu[0].header['imsize'] =  imsize
-    hdu[0].header['source_name'] =  source_name
-    hdu[0].header['ra'] =  ra
-    hdu[0].header['dec'] =  dec
-    hdu[0].header['numpix'] =  numpix
-
-    return hdu
-
-
-def get_stars (ra,dec,radius=3):
-
+def get_stars_optical(ra,dec,radius=3):
     stars = query_stars_gaia(ra,dec,radius=radius)
     if len(stars) != 0:
         print('Star catalog from Gaia')
@@ -134,8 +52,29 @@ def get_stars (ra,dec,radius=3):
         print('Failed to retrieve stars')
         return ''
         
-    # add offset columns
+    return stars
 
+def get_stars (ra,dec,radius=3,wv = 'optical'):
+    
+    if wv == 'optical':
+        stars = get_stars_optical(ra,dec,radius=3)
+    
+    elif wv == 'ir':
+        stars = get_stars_2mass(ra,dec,radius=radius)
+        if len(stars) != 0:
+            print('Star catalog from 2MASS')
+        
+        if len(stars) == 0:
+            print('Failed to retrieve stars from 2MASS, trying optical surveys')
+            stars = get_stars_optical(ra,dec,radius=3)
+    else:
+        print(f'{wv} wavelenght not supported')
+        return ''
+        
+    # add offset columns
+    if len(stars) == 0:
+        return ''
+        
     target = SkyCoord(ra*u.deg, dec*u.deg, frame="icrs")
     coords = SkyCoord(stars["ra"].values*u.deg,
                        stars["dec"].values*u.deg,
@@ -147,6 +86,8 @@ def get_stars (ra,dec,radius=3):
     stars["offset_NS_arcsec"] = ddec.to(u.arcsec).value
 
     return stars
+
+
 
 def fits2image_projected(hdu,pa_deg=0,imsize = 6,radius = 3):
 
@@ -170,10 +111,9 @@ def fits2image_projected(hdu,pa_deg=0,imsize = 6,radius = 3):
         right=0.95,
     )
     
-    # npixels = 60 * imsize / pixscale #= source_image_parameters[image_source].get("npixels", 256)
-    # pixscale = 60 * imsize / numpix
     
     wcs = WCS(naxis=2)
+    
     # set the headers of the WCS.
     # The center of the image is the reference point (source_ra, source_dec):
     wcs.wcs.crpix = [npixels / 2, npixels / 2]
@@ -242,15 +182,15 @@ def fits2image_projected(hdu,pa_deg=0,imsize = 6,radius = 3):
             krej=zscale_krej,
         )
         norm = ImageNormalize(im, vmin=vmin, vmax=vmax, interval=interval)
-        watermark = watermark #source_image_parameters[image_source]["str"]
+        watermark = watermark 
         fallback = False
-    
+    else:
+        print(im)
+        raise TypeError('Downloaded an empty image')
     # add the images in the top left corner
     ax = fig.add_subplot(spec[0, 0], projection=new_wcs)
     ax_text = fig.add_subplot(spec[0, 1])
     ax_text.axis("off")
-    # ax_starlist = fig.add_subplot(spec[1, 0:])
-    # ax_starlist.axis("off")
     
     ax.imshow(im, origin="lower", norm=norm, cmap="gray_r")
     ax.set_autoscale_on(False)
@@ -263,7 +203,6 @@ def fits2image_projected(hdu,pa_deg=0,imsize = 6,radius = 3):
     fontweight="bold",
     )
 
-    
     # plot hair cross for the target
 
     coord = SkyCoord(ra*u.deg, dec*u.deg, frame="icrs")
@@ -291,64 +230,69 @@ def fits2image_projected(hdu,pa_deg=0,imsize = 6,radius = 3):
                  1 - (0.5) / 4,
                  star_text,
                 color = color_target)
-    
+    ####
     # getting stars
-    # radius = 3
-    stars = get_stars(ra,dec,radius=radius)
-    top3 = stars.iloc[:3]
-    
-    colors = [
-    "#E69F00",  # orange
-    "#56B4E9",  # sky blue
-    "#009E73",  # bluish green
-    ]  # any colors you want
-
-    
-    top3_colors = {}
-    
-
-    arm_len = 30     # length of each arm (pixels)
-    gap = 15          # half-gap at center (pixels)
-    lw = 3
-
-
-    for i, (idx, row) in enumerate(top3.iterrows()):
-        coord = SkyCoord(row.ra*u.deg, row.dec*u.deg, frame="icrs")
-        x, y = new_wcs.world_to_pixel(coord)        
-        ra_str  = coord.ra.to_string(unit=u.hour, sep=":", precision=2)
-        dec_str = coord.dec.to_string(unit=u.deg, sep=":", precision=1, alwayssign=True)
-
-        mag = row.mag_g
-        offset_ra, offset_dec = row.offset_EW_arcsec,row.offset_NS_arcsec
-
-        c = colors[i]
-        top3_colors[idx] = c
+    stars = get_stars(ra,dec,radius=radius,wv=hdu[0].header['wv'])
+   
+    if len(stars) != 0:
         
-        star_text = (
-                rf"$\bf{{{source_name}-{str(i+1)}}}$  {mag:.2f} mag"
-                f'\n{coord.ra.deg:.6f} {coord.dec.deg:.6f}\n'
-                f'{ra_str} {dec_str} \n'
-                f'offset RA, Dec: {round(offset_ra,4) } {round(offset_dec,4)}'
-                f'\nRA > 0 East, Dec > 0 N'
-               )
+        top3 = stars.iloc[:3]
+        
+        colors = [
+        "#E69F00",  # orange
+        "#56B4E9",  # sky blue
+        "#009E73",  # bluish green
+        ]  # any colors you want
     
-        c = colors[i]
-        top3_colors[idx] = c
+        top3_colors = {}
+        
+        arm_len = 30     # length of each arm (pixels)
+        gap = 15          # half-gap at center (pixels)
+        lw = 3
     
-        # Horizontal arms
-        ax.plot([x - arm_len, x - gap], [y, y], color=c, lw=lw)
-        ax.plot([x + gap, x + arm_len], [y, y], color=c, lw=lw)
     
-        # Vertical arms
-        ax.plot([x, x], [y - arm_len, y - gap], color=c, lw=lw)
-        ax.plot([x, x], [y + gap, y + arm_len], color=c, lw=lw)
+        for i, (idx, row) in enumerate(top3.iterrows()):
+            coord = SkyCoord(row.ra*u.deg, row.dec*u.deg, frame="icrs")
+            x, y = new_wcs.world_to_pixel(coord)        
+            ra_str  = coord.ra.to_string(unit=u.hour, sep=":", precision=2)
+            dec_str = coord.dec.to_string(unit=u.deg, sep=":", precision=1, alwayssign=True)
     
-    # add text on the side
-        ax_text.text(0,
-                     1 - ((i+1) + 0.5) / 4,
-                     star_text,
-                     color = c)
-        print(star_text)
+            mag = row.mag
+            
+            offset_ra, offset_dec = row.offset_EW_arcsec,row.offset_NS_arcsec
+            EW, NS = 'E', 'N'
+            if offset_ra < 0 : 
+                EW = 'W'
+            if offset_dec < 0 : 
+                NS = 'S'
+                
+            c = colors[i]
+            top3_colors[idx] = c
+            
+            star_text = (
+                    rf"$\bf{{{source_name}-{str(i+1)}}}$  {mag:.2f} mag g-band "
+                    # f'\n{coord.ra.deg:.6f} {coord.dec.deg:.6f}\n'
+                    f'\n {ra_str} {dec_str} \n'
+                    f'offset RA, Dec: {round(np.abs(offset_ra),2)} {EW} {round(np.abs(offset_dec),2)} {NS}'
+                   )
+        
+            c = colors[i]
+            top3_colors[idx] = c
+        
+            # Horizontal arms
+            ax.plot([x - arm_len, x - gap], [y, y], color=c, lw=lw)
+            ax.plot([x + gap, x + arm_len], [y, y], color=c, lw=lw)
+        
+            # Vertical arms
+            ax.plot([x, x], [y - arm_len, y - gap], color=c, lw=lw)
+            ax.plot([x, x], [y + gap, y + arm_len], color=c, lw=lw)
+        
+        # add text on the side
+            ax_text.text(0,
+                         1 - ((i+1) + 0.5) / 4,
+                         star_text,
+                         color = c)
+            print(star_text)
 
     
     # compass rose
@@ -391,6 +335,7 @@ def parse_args():
     parser.add_argument( "--source-name", default="", help="Source name (string)")
     parser.add_argument( "--pa-deg", type=float, default=0.0, help="Position angle in degrees (East of North)")
     parser.add_argument( "--imsize", type=float, default=4.0, help="Image size in arcminutes")
+    parser.add_argument( "--wv", type=str, default='optical', help="Wavelenght regime: 'optical' or 'ir' ")
     parser.add_argument("--radius", type=float, default=1.0, help="Radius in arcminutes")
     parser.add_argument("--output-folder", type=str, default='./finder_charts/', help="output folder")
 
@@ -411,6 +356,14 @@ def main():
     output_folder = args.output_folder
     Path(output_folder).mkdir(parents=True, exist_ok=True)
 
+    logger = setup_logger(
+        name="finder chart",
+        logfile="./finder.log"
+    )
+
+    logger.info("Pipeline started")
+    logger.info([source_name, pa_deg, imsize, radius, output_folder, args.wv])
+    
     
     # convert to astropy coords
     ra,dec = parse_coords(ra_,dec_)
@@ -426,17 +379,36 @@ def main():
     output_file = output_folder + f'finder_{source_name}.pdf'
     print(f"Output file: {output_file} ")
     
-    
-    
-    
     # download image
-    
-    hdu = get_image_ls(ra,dec,source_name,imsize = imsize)
-    
-    if hdu == '':
-        print('image from PS1 a bit distorted, offsets are ok')
-        hdu = get_image_ps1(ra,dec,source_name,imsize = imsize)
-    
+    if args.wv == 'optical':
+        hdu = get_image_fallbacks(ra,dec,source_name,imsize = imsize)
+        
+        # add wv to decide if query stars from 2mass or Gaia
+        hdu[0].header['wv'] = args.wv
+            
+    if args.wv == 'ir':
+        try:
+            hdu = get_image_2mass(ra,dec,source_name,imsize = imsize)
+        
+            if not (len(hdu) == 0):
+                im = hdu[0].data
+                cent = int(len(im) / 2)
+                width = int(0.05 * len(im))
+                test_slice = slice(cent - width, cent + width)
+                all_nans = np.isnan(im[test_slice, test_slice].flatten()).all()
+                all_zeros = (im[test_slice, test_slice].flatten() == 0).all()
+                if not (all_zeros or all_nans):
+                    print('image from 2MASS')
+                    hdu[0].header['wv'] = args.wv
+            else:
+                print("could not get 2MASS image, trying optical surveys ")
+                hdu = get_image_fallbacks(ra,dec,source_name,imsize = imsize)
+                hdu[0].header['wv'] = 'optical'
+        except:
+            print("could not get 2MASS image, trying optical surveys ")
+            hdu = get_image_fallbacks(ra,dec,source_name,imsize = imsize)
+            hdu[0].header['wv'] = 'optical'
+            
     fig = fits2image_projected(hdu,pa_deg=pa_deg, imsize = imsize,radius = radius)
     
     fig.savefig(
--- utils.py
+++ utils.py
@@ -40,6 +40,67 @@ from reproject import reproject_interp
 # ------------------
 mpl.rcParams["font.size"] = 15  # default = 10.0
 
+# todo - the finder when applied PA is turned the other way around
+
+# Standard library
+import io
+import os
+import math
+import re
+from io import BytesIO
+from pathlib import Path
+
+# Third-party
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import requests
+
+# Astropy
+import astropy.units as u
+from astropy.coordinates import SkyCoord, Angle
+from astropy.io import fits
+from astropy.time import Time
+from astropy.wcs import WCS
+from astropy.visualization import ImageNormalize, ZScaleInterval
+
+# Reproject
+from reproject import reproject_interp
+
+from astroquery.skyview import SkyView
+from astropy.coordinates import SkyCoord
+import astropy.units as u
+
+
+# # utils
+# from utils import query_stars_gaia, query_stars_ps1, query_stars_ls, get_stars_2mass
+# from utils import get_url, parse_coords
+
+
+import logging
+from pathlib import Path
+
+def setup_logger(
+    name="myapp",
+    logfile="myapp.log",
+    level=logging.INFO,
+):
+    log_path = Path(logfile)
+    log_path.parent.mkdir(parents=True, exist_ok=True)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+
+    # Prevent duplicate handlers if called multiple times
+    if not logger.handlers:
+        file_handler = logging.FileHandler(log_path, mode="a")  # append
+        formatter = logging.Formatter(
+            "%(asctime)s | %(levelname)s | %(message)s"
+        )
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+    return logger
 
 
 def parse_coords(ra_str, dec_str):
@@ -113,7 +174,7 @@ def query_stars_gaia(ra,dec,radius = 3):
     df = results.to_pandas()
     df.keys()
     df = df.rename(columns={
-        "phot_g_mean_mag": "mag_g",
+        "phot_g_mean_mag": "mag",
             })
 
     return df
@@ -127,7 +188,7 @@ def query_stars_ps1(ra,dec,radius = 3):
         dec: Dec in deg
         radius: search radius in arcmin
     returns
-        df: Pandas DataFrame with ra, dec, mag_g, mag_r and others (rKronMag,
+        df: Pandas DataFrame with ra, dec, mag (mag_g), mag_r and others (rKronMag,
 qualityFlag)
     '''
     
@@ -157,7 +218,7 @@ qualityFlag)
     df = df.rename(columns={
     "raMean": "ra",
     "decMean": "dec",
-    "gPSFMag": "mag_g",
+    "gPSFMag": "mag",
     "rPSFMag": "mag_r"
         })
     
@@ -178,7 +239,7 @@ def query_stars_ls(ra,dec,radius = 6):
         dec: Dec in deg
         radius: size of the box in arcmin
     returns
-        df: Pandas DataFrame with ra, dec, mag_g, mag_r, mag_z
+        df: Pandas DataFrame with ra, dec, mag (mag_g), mag_r, mag_z
     '''
     
     
@@ -204,11 +265,263 @@ def query_stars_ls(ra,dec,radius = 6):
     results = job.to_table()
     
     df = results.to_pandas()
+    df = df.rename(columns={
+        "mag_g": "mag",
+            })
+    
+    
+    return df
+
+def get_stars_2mass(ra,dec,radius=2):
+
+    from astroquery.irsa import Irsa
+    from astropy.coordinates import SkyCoord
+    import astropy.units as u
+    
+    # Target position
+    target = SkyCoord(ra*u.deg, dec*u.deg, frame="icrs")
+        
+    # Query 2MASS Point Source Catalog
+    tbl = Irsa.query_region(
+        target,
+        radius=radius* u.arcmin,
+        catalog="fp_psc"   # 2MASS Point Source Catalog
+    )
+    
+    # Extract J-band photometry
+    tbl = tbl["ra", "dec", "j_m", "j_cmsig", "ph_qual", "cc_flg"]
+    
+    good = np.array([ph_qa[0] =='A' or ph_qa[0] =='B' for ph_qa in tbl["ph_qual"]]) #* np.array([ph_qa[0] =='0' for ph_qa in tbl["cc_flg"]])    
+    
+    clean = tbl[good]
+    
+    df = clean.to_pandas()
+    df = df.rename(columns={
+        "j_m": "mag",
+            })
     
     
     return df
 
 
+def get_image_ps1(ra,dec,source_name,imsize=6):
+    
+    url = (
+            f"https://alasky.cds.unistra.fr/hips-image-services/hips2fits"
+            f"?width=500&height=500&fov={imsize/60}&ra={ra}&dec={dec}"
+            f"&hips=CDS/P/PanSTARRS/DR1/r"
+        )
+    
+    response = requests.get(url, timeout=30)
+    hdu = fits.open(BytesIO(response.content))
+    
+    npixels = len(hdu[0].data)
+    pixscale = imsize*60/npixels
+    
+    if response is None or response.status_code != 200:
+            print('failed PS1 retrieval')
+            return ''
+        
+        
+    hdu = fits.open(BytesIO(response.content))
+    if len(hdu) == 0:
+        print('failed PS1 retrieval')
+        return ''
+        
+    hdu[0].header['watermark'] =  'PS1'
+    hdu[0].header['pixscale'] =  pixscale
+    hdu[0].header['imsize'] =  imsize
+    hdu[0].header['source_name'] =  source_name
+    hdu[0].header['ra'] =  ra
+    hdu[0].header['dec'] =  dec
+    hdu[0].header['numpix'] = npixels
+
+    return hdu
 
 
+def get_image_ls(ra,dec,source_name,
+                  imsize = 6# in arcmin
+                 ):
+    
+    pixscale = 0.26
+    LS_CUTOUT_TIMEOUT = 30
+    
+    numpix = math.ceil(60 * imsize / pixscale)
+    
+    ls_query_url = (
+         f"http://legacysurvey.org/viewer/fits-cutout/"
+         f"?ra={ra}&dec={dec}&layer=dr8&pixscale={pixscale}&bands=r&size={numpix}"
+    )
+    print(ls_query_url)
+    
+    response = requests.get(ls_query_url, timeout=LS_CUTOUT_TIMEOUT)
+    if response is None or response.status_code != 200:
+        print('failed')
+        return ''
+    
+    
+    hdu = fits.open(BytesIO(response.content))
+    if len(hdu) == 0:
+        print('failed')
+        return ''
+        
+    hdu[0].header['watermark'] =  'LS'
+    hdu[0].header['pixscale'] =  pixscale
+    hdu[0].header['imsize'] =  imsize
+    hdu[0].header['source_name'] =  source_name
+    hdu[0].header['ra'] =  ra
+    hdu[0].header['dec'] =  dec
+    hdu[0].header['numpix'] =  numpix
 
+    return hdu
+
+def get_image_decaps(ra,dec,source_name,
+                  imsize = 6# in arcmin
+                 ):
+    
+    pixscale = 0.26
+    DECAPS_CUTOUT_TIMEOUT = 30
+    
+    numpix = math.ceil(60 * imsize / pixscale)
+    
+    decaps_query_url = (
+         f"http://legacysurvey.org/viewer/fits-cutout/?layer=decaps2&"
+         f"ra={ra}&dec={dec}&pixscale={pixscale}&bands=r&size={numpix}"
+    )
+    print(decaps_query_url)
+    
+    response = requests.get(decaps_query_url, timeout=DECAPS_CUTOUT_TIMEOUT)
+    if response is None or response.status_code != 200:
+        print('failed')
+        return ''
+    
+    
+    hdu = fits.open(BytesIO(response.content))
+    if len(hdu) == 0:
+        print('failed')
+        return ''
+        
+    hdu[0].header['watermark'] =  'LS'
+    hdu[0].header['pixscale'] =  pixscale
+    hdu[0].header['imsize'] =  imsize
+    hdu[0].header['source_name'] =  source_name
+    hdu[0].header['ra'] =  ra
+    hdu[0].header['dec'] =  dec
+    hdu[0].header['numpix'] =  numpix
+
+    return hdu
+
+def get_image_dss(ra,dec,source_name,
+                  imsize = 6# in arcmin
+                 ):
+    
+    url = f"http://archive.stsci.edu/cgi-bin/dss_search?v=poss2ukstu_red&r={ra}&dec={dec}&h={imsize}&w={imsize}&e=J2000"
+    response = requests.get(url, timeout=30)
+    hdu = fits.open(BytesIO(response.content))
+    
+    npixels = len(hdu[0].data)
+    imsize = imsize
+    pixscale = imsize*60/npixels
+    
+    if response is None or response.status_code != 200:
+            print('failed DSS retrieval')
+            return ''
+        
+        
+    hdu = fits.open(BytesIO(response.content))
+    if len(hdu) == 0:
+        print('failed DSS retrieval')
+        return ''
+        
+    hdu[0].header['watermark'] =  'DSS'
+    hdu[0].header['pixscale'] =  pixscale
+    hdu[0].header['imsize'] =  imsize
+    hdu[0].header['source_name'] =  source_name
+    hdu[0].header['ra'] =  ra
+    hdu[0].header['dec'] =  dec
+    hdu[0].header['numpix'] =  len(hdu[0].data)
+
+    return hdu
+
+def get_image_2mass(ra,dec,source_name,
+                  imsize = 6# in arcmin
+                 ):
+    
+    url = (
+    "https://skyview.gsfc.nasa.gov/current/cgi/runquery.pl?"
+    f"Position={ra},{dec}"
+    "&Survey=2MASS-J"
+    f"&Radius={imsize/60}"   # degrees = 5 arcmin
+    "&Return=FITS"
+    )
+
+    response = requests.get(url, timeout=30)
+    hdu = fits.open(BytesIO(response.content))
+    
+    npixels = len(hdu[0].data)
+    pixscale = imsize*60/npixels
+    
+    if response is None or response.status_code != 200:
+            print('failed 2MASS retrieval')
+            return ''
+        
+        
+    hdu = fits.open(BytesIO(response.content))
+    im = hdu[0].data
+    cent = int(npixels / 2)
+    width = int(0.05 * npixels)
+    test_slice = slice(cent - width, cent + width)
+    all_nans = np.isnan(im[test_slice, test_slice].flatten()).all()
+    all_zeros = (im[test_slice, test_slice].flatten() == 0).all()
+  
+    if len(hdu) == 0 or all_zeros or all_nans:
+        print('failed 2MASS retrieval, trying again')
+        
+        coord = SkyCoord(ra*u.deg, dec*u.deg, frame="icrs")
+        images = SkyView.get_images(
+            position=coord,
+            survey=["2MASS-J"],   # 2MASS-J, 2MASS-H, 2MASS-K
+            radius=imsize * u.arcmin,
+            pixels=npixels
+        )
+        hdu = images[0]
+        im = hdu[0].data
+        all_nans = np.isnan(im[test_slice, test_slice].flatten()).all()
+        all_zeros = (im[test_slice, test_slice].flatten() == 0).all()
+        if len(im) == 0 or all_zeros or all_nans:
+            return ''
+        
+    hdu[0].header['watermark'] =  '2MASS'
+    hdu[0].header['pixscale'] =  pixscale
+    hdu[0].header['imsize'] =  imsize
+    hdu[0].header['source_name'] =  source_name
+    hdu[0].header['ra'] =  ra
+    hdu[0].header['dec'] =  dec
+    hdu[0].header['numpix'] = npixels
+
+    return hdu
+
+def get_image_fallbacks(ra,dec,source_name,imsize = 5):
+    hdu = get_image_ls(ra,dec,source_name,imsize = imsize)
+    if hdu != '':
+        print('image from LS')
+    
+    if hdu == '':
+        hdu = get_image_ps1(ra,dec,source_name,imsize = imsize)
+        if hdu != '':
+            print('image from PS1')
+        
+    if hdu == '':
+        hdu = get_image_decaps(ra,dec,source_name,imsize = imsize)
+        if hdu != '':
+            print('image from DECaPs')
+
+    if hdu == '':
+        hdu = get_image_dss(ra,dec,source_name,imsize = imsize)
+        if hdu != '':
+            print('image from DSS')
+    
+    if hdu == '':
+        raise TypeError("could not get image, tried LS, PS1, DECaPs, and DSS")
+    
+    return hdu
